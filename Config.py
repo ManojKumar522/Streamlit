b6420b0ad9dc25d5fce0fd35ad0cf750cad73170
Snowflake-Anblicks <115771546+Snowflake-Anblicks@users.noreply.github.com>
2022-10-28 12:44:06 +0530

    Delete Config.py

--- Config.py
+++
@@ -1,9 +0,0 @@
-connection_parameters = {
-    "account": "xxxxxxxxxxxxx",
-    "user": "xxxxxx",
-    "password": "xxxxxxx",
-    "role": "xxxxxxxx",
-    "warehouse": "xxxxxxx",
-    "database": "xxxxxxx",
-    "schema": "xxxxxxxxx"
-    }
