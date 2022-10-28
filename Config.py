243c4bc3f78188da216d076939dc9f2857c70daa
Snowflake-Anblicks <115771546+Snowflake-Anblicks@users.noreply.github.com>
2022-10-28 12:39:42 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,9 @@
 connection_parameters = {
-    "account": "anblickspartner.east-us-2.azure",
-    "user": "mkumar",
-    "password": "Amumanu0729@",
-    "role": "ACCOUNTADMIN",
-    "warehouse": "DEV_WH",
-    "database": "CUSTOMERAI",
-    "schema": "MAIN"
+    "account": "xxxxxxxxxxxxx",
+    "user": "xxxxxx",
+    "password": "xxxxxxx",
+    "role": "xxxxxxxx",
+    "warehouse": "xxxxxxx",
+    "database": "xxxxxxx",
+    "schema": "xxxxxxxxx"
     }
